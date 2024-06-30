961d4edd0fe52963f4aff09dee344f7733740253
Sxinar <Sxinar@users.noreply.github.com>
2024-06-30 13:22:19 +0000

    deploy: 9325d9e7d6c682fda987412a4824a468f4f84929

---
+++ link/script.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggle = document.getElementById('theme-toggle');
+  const currentTheme = localStorage.getItem('theme') || 'dark';
+
+  if (currentTheme === 'light') {
+    document.body.classList.add('light-mode');
+    themeToggle.checked = true;
+  }
+
+  themeToggle.addEventListener('change', function () {
+    if (this.checked) {
+      document.body.classList.add('light-mode');
+      localStorage.setItem('theme', 'light');
+    } else {
+      document.body.classList.remove('light-mode');
+      localStorage.setItem('theme', 'dark');
+    }
+  });
+});
